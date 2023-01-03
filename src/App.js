045e5005a6ed3406b3f227b7ba7bf51e569caaf5
juliajulia89 <julkajanicka@gmail.com>
2023-01-03 16:23:55 +0100

    style touch up

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"> </div>
-        <div className="current-operand"> </div>
+        <div className="previous-operand">gtrgrggerg </div>
+        <div className="current-operand">grgregergregr </div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
